a797b04daf62f5adc3bc2077c6fd57a0794e2405
Oreeeee <oree3ee@gmail.com>
2025-05-03 03:03:56 +0200

    Create a checker for dynamic code building support

--- src/Bass/Shared/ChannelReferences.cs
+++ src/Bass/Shared/ChannelReferences.cs
@@ -22,7 +22,7 @@ namespace ManagedBass
         {
 #if !__IOS__
             // in .NET iOS, the __IOS__ constant cannot be seen, so rely on RuntimeFeature instead.
-            if (!RuntimeFeature.IsDynamicCodeSupported)
+            if (!DynamicCodeHelper.Supported)
                 return;
 
             if (proc == null)
@@ -51,7 +51,7 @@ namespace ManagedBass
         {
 #if !__IOS__
             // in .NET iOS, the __IOS__ constant cannot be seen, so rely on RuntimeFeature instead.
-            if (!RuntimeFeature.IsDynamicCodeSupported)
+            if (!DynamicCodeHelper.Supported)
                 return;
 
             var key = Tuple.Create(Handle, SpecialHandle);
---
+++ src/Bass/Shared/DynamicCodeHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ManagedBass
+{
+    public static class DynamicCodeHelper
+    {
+        public static bool Supported
+        {
+            // Check is dynamic code supported by trying to create dynamic code
+            get
+            {
+                try
+                {
+                    var assemblyName = new System.Reflection.AssemblyName("DynamicAssembly");
+                    var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, System.Reflection.Emit.AssemblyBuilderAccess.Run);
+                    var moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
